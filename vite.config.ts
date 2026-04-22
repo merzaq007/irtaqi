afa0a6aefc7b6e93b83bcd007bdd8f554c1ed84b
merzaq007 <merzaq007@github.com>
2026-04-22 20:23:00 +0100

    fix: set vite base to '/' to fix white screen on Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -206,7 +206,7 @@ function cdnPrefixImages(): Plugin {
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/irtaqi/' : '/',
+    base: '/',
     server: {
       host: "::",
       port: 8080,
